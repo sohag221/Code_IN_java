195dad7da9ab68fadcbb6f7b155aaa0663197490
Md Sohag <110124971+sohag221@users.noreply.github.com>
2022-08-01 12:28:23 +0600

    Add files via upload

---
+++ sorting_bys_ARRAYLIST.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class sorting_bys_ARRAYLIST {
+    public static void main(String[] args) {
+        ArrayList<Integer> number=new ArrayList<>();
+        int m;
+        Scanner input=new Scanner(System.in);
+        for (int i=1;i<=5;i++){
+        number.add(m=input.nextInt());
+        }
+        System.out.println(number);
+        Collections.sort(number);
+        System.out.println("aferet sorting\n"+number);
+        Collections.reverse(number);
+        System.out.println(number);
+    }
+}
