eb48f55729ccb6ba97bd81166421046508ad24e2
Md Sohag <110124971+sohag221@users.noreply.github.com>
2022-08-20 14:45:27 +0600

    Update recursion.java

--- recursion.java
+++ recursion.java
@@ -1,18 +1,16 @@
 public class recursion {
     int fact(int number){
-        if (number==1){
+        if (number==1 && number==0){
             return 1;
         }
         else {
             return number*fact(number-1);
         }
     }
-
     public static void main(String[] args) {
         recursion sohag=new recursion();
         sohag.fact(5);
     }
-
-    }
+     }
 
 
